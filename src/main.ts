470b16af54779f2293f5f745bb4295a2ef305d79
Wahyu Setiawan <whysetiawan27@gmail.com>
2021-12-02 16:58:24 +0700

    Fix package.json for heroku

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const docsConfig = new DocumentBuilder()
+    .setTitle('CAYN API Documentation')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, docsConfig);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();

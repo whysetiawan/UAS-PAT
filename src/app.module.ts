aea6e993e3a1b2d38c754b9d2953275be6c3aa4e
Wahyu Setiawan <whysetiawan27@gmail.com>
2021-12-04 10:30:56 +0700

    Integrating Typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+// import db from './app.db_connection';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb+srv://db-admin:x9NV7rLzF1z5Pvp7@cayn.yusnl.mongodb.net/cayndb?retryWrites=true&w=majority',
+      // entities: [join(__dirname, '**/**.entity{.ts,.js}')],
+      logging: true,
+      useNewUrlParser: true,
+      synchronize: true,
+      ssl: true,
+      authSource: 'db-admin',
+      // database: 'myFirstDatabase',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,24 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: console,
+  });
+  const port = process.env.PORT ?? 3000;
   const docsConfig = new DocumentBuilder()
     .setTitle('CAYN API Documentation')
     .setVersion('1.0')
     .build();
 
   const document = SwaggerModule.createDocument(app, docsConfig);
-  SwaggerModule.setup('api/docs', app, document);
+  SwaggerModule.setup('docs', app, document);
 
+<<<<<<< Updated upstream
   await app.listen(3000);
+=======
+  app.setGlobalPrefix('api/v1');
+  await app.listen(port);
+  console.log('listening to port', port);
+>>>>>>> Stashed changes
 }
 bootstrap();

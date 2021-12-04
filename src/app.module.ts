f3093d1d7abc92a1a46d7504f04eccb416921751
Wahyu Setiawan <whysetiawan27@gmail.com>
2021-12-04 12:28:26 +0700

    Integrated MongoDb with TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,19 +6,7 @@ import { join } from 'path';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mongodb',
-      url: 'mongodb+srv://db-admin:OgMtn5XoDc4fg4Sa@cayn.yusnl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-      authSource: 'admin',
-      // useNewUrlParser: true,
-      synchronize: true,
-      logging: true,
-      useUnifiedTopology: true,
-      replicaSet: 'cayn-shard-0',
-      // entities: [],
-    }),
-  ],
+  imports: [TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
